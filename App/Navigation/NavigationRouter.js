f028c44d3437c74e59c1d0e9ceff95ffdd19ed88
hitesh <hitesh@bekground.com>
2017-04-10 14:54:18 +0530

    Android check

--- App/Navigation/NavigationRouter.js
+++ App/Navigation/NavigationRouter.js
@@ -42,7 +42,7 @@ class NavigationRouter extends Component {
             <Scene key='login' component={LoginScreen} title='PT SPOTTER' hideNavBar />
             <Scene key='signup' component={SignUpScreen} title='PT SPOTTER' hideNavBar />
             <Scene key='mobile' component={MobileVerification} title='Verification' hideNavBar/>
-            <Scene initial key='homeScreen' component={HomeScreen} title='HOME' hideNavBar={false} navBar={CustomNavBar} />
+            <Scene  key='homeScreen' component={HomeScreen} title='HOME' hideNavBar={false} navBar={CustomNavBar} />
             <Scene key='clientHome' component={ClientHome} title='HOME' hideNavBar={false} navBar={CustomNavBar} />
             <Scene key='postDetail' component={PostDetail} title='POST' hideNavBar />
             <Scene key='editProfile' component={EditProfile} title='Edit Profile' hideNavBar />
@@ -50,7 +50,7 @@ class NavigationRouter extends Component {
             <Scene key='clientsettings' component={ClientSettings} title='SETTINGS' hideNavBar={false} navBar={CustomNavBar}/>
             <Scene key='availability' component={Availability} title='AVAILABILITY' hideNavBar/>
             <Scene key='sessions' component={Sessions} title='SESSIONS' hideNavBar/>
-            <Scene key='SetupSessions' component={SetupSessions} title='SETUP SESSIONS'  hideNavBar={true}/>
+            <Scene initial key='SetupSessions' component={SetupSessions} title='SETUP SESSIONS'  hideNavBar={true}/>
             <Scene key='clientDetails' component={ClientDetail} title='' hideNavBar/>
             <Scene key='trainerDetails' component={TrainerDetail} title='' hideNavBar/>
             <Scene key='ratingScreen' component={RatingScreen} title='Rating' hideNavBar/>
